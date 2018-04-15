7bd6ab103c648c71ecdfb0d5178b44cb1a0b7890
超 张 <imagine243@gmail.com>
2018-04-15 22:06:43 +0800

    修改

--- lexer.go
+++ lexer.go
@@ -1,1 +1,18 @@
 package tinygolexer
+
+type Lexer struct {
+	src    []rune
+	tokens *[]Token
+	index  int
+}
+
+func (self *Lexer) Lex(s string) {
+	self.src = []rune(s)
+	self.tokens = new([]Token)
+}
+func NewLexer() *Lexer {
+
+	return &Lexer{
+		index: 0,
+	}
+}
